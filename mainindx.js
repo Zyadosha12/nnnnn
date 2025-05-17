f34ef57a7150acc6b2d74eca288e9431e56ea4ca
Zyadosha12 <zyad62951@gmail.com>
2025-05-18 01:38:53 +0300

    Add files via upload

--- mainindx.js
+++ mainindx.js
@@ -1,7 +1,7 @@
 let ok = document.getElementById("ok");
 let container = document.getElementById("container");
 let heading = document.getElementById("heading");
-const form = document.getElementById("muForm");
+const form = document.getElementById("myForm");
 ok.onclick = function () {
     ok.style.display = "none"
     heading.style.display = "none"
@@ -16,3 +16,23 @@ function Check() {
         alert(' يجب ملئ جميع المدخلات')
     }
 }
+// const form = document.getElementById("myForm");
+form.addEventListener("submit", function (e) {
+    e.preventDefault(); // يمنع الإرسال التلقائي
+    const formData = new FormData(form);
+    fetch("https://formsubmit.co/zyad62951@gmail.com", {
+        method: "POST",
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                // لو تم الإرسال بنجاح، نروح لصفحة Home.html
+                window.location.href = "Home.html";
+            } else {
+                alert("حدث خطأ في الإرسال");
+            }
+        })
+        .catch(error => {
+            alert("لم يتم الإرسال. حاول مرة أخرى.");
+        });
+});
